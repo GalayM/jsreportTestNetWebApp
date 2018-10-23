7d566857607c3d30c2a3847af8e059e1dfde74de
Galay Mihail <m.galay@itransition.com>
2018-10-23 03:30:07 +0300

    Init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using jsreport.MVC;
+using jsreport.Types;
+using System.Web.Mvc;
+
+namespace NetWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [EnableJsReport]
+        public ActionResult Index()
+        {
+            var jsReportFeature = HttpContext.JsReportFeature()
+                .Recipe(Recipe.ChromePdf)
+                .Engine(Engine.Handlebars)
+                .Configure(r => r.Template.Chrome = new Chrome
+                {
+                    HeaderTemplate = this.RenderViewToString("Header", new { }),
+                    DisplayHeaderFooter = true,
+                    MarginTop = "2cm",
+                    MarginLeft = "1cm",
+                    MarginBottom = "2cm",
+                });
+            jsReportFeature.RenderRequest.Template.Helpers = @"function getThisJson() {return JSON.stringify(this);};";
+
+            return View("TestPage");
+        }
+    }
+}
